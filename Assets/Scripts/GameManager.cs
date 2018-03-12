fe520f25b840d67c88c57fcf407e946adc11a6f9
Ignas <ignas.bagdon@gmail.com>
2018-03-12 21:25:43 +0100

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+	[SerializeField] private GameObject spawnPoint;
+	[SerializeField] private GameObject[] enemies;
+	[SerializeField] private int maxEnemiesOnScreen;
+	[SerializeField] private int totalEnemies;
+	[SerializeField] private int enemiesPerSpawn;
+
+	const float spawnDelay = 0.5f;
+
+	private int enemiesOnScreen = 0;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(spawn());
+	}
+
+	IEnumerator spawn() {
+		if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) {
+			for (int i = 0; i < enemiesPerSpawn; i++) {
+				if (enemiesOnScreen < maxEnemiesOnScreen) {
+					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnScreen++;
+				}
+			}
+			yield return new WaitForSeconds(spawnDelay);
+			StartCoroutine(spawn());
+		}
+	}
+
+	public void removeEnemyFromScreen() {
+		if (enemiesOnScreen > 0) {
+			enemiesOnScreen--;
+		}
+	}
+}
